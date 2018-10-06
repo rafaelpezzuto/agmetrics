0eadbbf241bc52f99b62d945728ca3226bfad7a0
Rafael Pezzuto <rafael.pezzuto@gmail.com>
2018-10-06 20:16:26 -0300

    add lineage by node_code

--- graph.py
+++ graph.py
@@ -5,23 +5,43 @@ class Node:
 		self.children = []
 		self.down_adjacent_list = []
 		self.up_adjacent_list = []
+		self.metrics = {}
+		self.lineage = ''
+		self.lineage_distance = None
+		self.other_attributes = {}
 
 	def __str__(self):
-		return str(self.code)
+		return 'node_' + str(self.code)
 
 	def __repr__(self):
-		return str(self.code)
+		return 'node_' + str(self.code)
+	
+	def get_full_str(self, metrics_header, other_attributes_header):
+		str_metrics = ','.join([str(self.metrics[k]) for k in metrics_header])
+		str_other_attributes = ','.join([self.other_attributes[k] for k in other_attributes_header])
+		if len(other_attributes_header) > 0:
+			return ','.join([self.code, str_other_attributes, self.lineage, str(self.lineage_distance), str_metrics])
+		else:
+			return ','.join([self.code, self.lineage, str(self.lineage_distance), str_metrics])
 
 class Edge:
-	def __init__(self, source_node, target_node):
+	def __init__(self, source_node, target_node, other_attributes={}):
 		self.source = source_node
 		self.target = target_node
+		self.other_attributes = other_attributes
 
 	def __str__(self):
 		return '->'.join([self.source.code, self.target.code])
 
 	def __repr__(self):
 		return '->'.join([self.source.code, self.target.code])
+
+	def get_full_str(self, other_attributes_header):
+		str_other_attributes = ','.join([self.other_attributes[k] for k in other_attributes_header])
+		if len(other_attributes_header) > 0:
+			return ','.join([self.source.code, self.target.code, str_other_attributes])
+		else:
+			return ','.join([self.source.code, self.target.code])
 
 class Graph:
 	def __init__(self):
@@ -31,14 +51,14 @@ class Graph:
 	def add_node(self, code):
 		self.vertices[code] = Node(code)
 
-	def add_edge(self, source_code, target_code):
+	def add_edge(self, source_code, target_code, other_attributes={}):
 		source_node = self.vertices.get(source_code)
 		target_node = self.vertices.get(target_code)
 		self.vertices[source_code].children.append(self.vertices[target_code])
 		self.vertices[target_code].parents.append(self.vertices[source_code])
-		self.edges.add(Edge(source_node, target_node))
-		source_node.down_adjacent_list.append(Edge(source_node, target_node))
-		target_node.up_adjacent_list.append(Edge(source_node, target_node))
+		self.edges.add(Edge(source_node, target_node, other_attributes))
+		source_node.down_adjacent_list.append(Edge(source_node, target_node, other_attributes))
+		target_node.up_adjacent_list.append(Edge(source_node, target_node, other_attributes))
 
 	def in_degree(self, node_code):
 		return len(self.vertices[node_code].parents)
@@ -122,7 +142,7 @@ class Graph:
 					stack.append(c)
 		return len(relationships)
 
-	def generations(self, node_code):
+	def generations(self, node_code, return_lenght=True):
 		height = 0
 		stack = [(self.vertices[node_code], height)]
 		visited = []
@@ -137,9 +157,12 @@ class Graph:
 						generations[height] = [c]
 					else:
 						generations[height].append(c)
-		return len(generations)
+		if return_lenght:
+			return len(generations)
+		else:
+			return generations
 
-	def inverse_generations(self, node_code):
+	def inverse_generations(self, node_code, return_lenght=True):
 		height = 0
 		stack = [(self.vertices[node_code], height)]
 		visited = []
@@ -154,7 +177,10 @@ class Graph:
 						generations[height] = [c]
 					else:
 						generations[height].append(c)
-		return len(generations)
+		if return_lenght:
+			return len(generations)
+		else:
+			return generations
 
 	def cousins(self, node_code):
 		v_inverse_fecundity = self.vertices[node_code].parents
