acdb1c74d7f0d602bb73a427476ac2edb5cd9aa0
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-15 22:28:28 -0300

    Generaliza parâmetros para calcular metricas em vários grafos

--- bash_calculate_metrics.sh
+++ bash_calculate_metrics.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+SUBGRAPHS_DIR=$1;
 for year in $(seq 1900 2019); do
-    base='/home/rafael/Temp/thesis/subgraphs/ano_acumulado/'
-    file_node=$base'nodes_1900_'$year'.csv'
-    file_edge=$base'edges_1900_'$year'.csv'
-    file_out=$base'metrics_1900_'$year'.csv'
+    base=$SUBGRAPHS_DIR;
+    file_node=$base'nodes_1900_'$year'.csv';
+    file_edge=$base'edges_1900_'$year'.csv';
+    file_out=$base'metrics_1900_'$year'.csv';
     if [ -f "$file_node" ]; then
-        ./calculate_metrics.py $file_node $file_edge $file_out 
+        ./calculate_metrics.py $file_node $file_edge $file_out;
     else
-        echo "$year does not exist"
+        echo "$year does not exist";
     fi
 done
